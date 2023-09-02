335548623ffd8562132244a117f192594cbc6a4b
mnengineer <masashiii0115@gmail.com>
2023-09-02 13:54:43 +0900

    feat: ImagePickerパッケージの使用

--- lib/widgets/user_image_picker.dart
+++ lib/widgets/user_image_picker.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
   const UserImagePicker({Key? key}) : super(key: key);
@@ -8,17 +12,34 @@ class UserImagePicker extends StatefulWidget {
 }
 
 class _UserImagePickerState extends State<UserImagePicker> {
+  File? _pickedImageFile;
+
+  void _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _pickedImageFile = File(pickedImage!.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         CircleAvatar(
           radius: 40,
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          backgroundImage: null,
+          backgroundColor: Colors.grey,
+          foregroundImage:
+              _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
         ),
         TextButton.icon(
-          onPressed: () {},
+          onPressed: _pickImage,
           icon: const Icon(Icons.image),
           label: Text(
             'Add Image',
