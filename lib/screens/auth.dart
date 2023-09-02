2b4a1682f8690054bc83bcaf10dc9d64e020cc47
mnengineer <masashiii0115@gmail.com>
2023-09-02 12:54:11 +0900

    feat: Login処理実装

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -25,28 +25,30 @@ class _AuthScreenState extends State<AuthScreen> {
     }
     _form.currentState!.save();
 
-    if (_isLogin) {
-      // Log user in
-    } else {
-      try {
+    try {
+      if (_isLogin) {
+        final userCredentials = await _firebase.signInWithEmailAndPassword(
+          email: _enterdEmail,
+          password: _enterdPassword,
+        );
+      } else {
         final userCredentials = await _firebase.createUserWithEmailAndPassword(
           email: _enterdEmail,
           password: _enterdPassword,
         );
-        print(userCredentials);
-      } on FirebaseAuthException catch (error) {
-        var message = 'Authentication failed.';
-        if (error.code == 'email-already-in-use') {
-          // ...
-        }
-        ScaffoldMessenger.of(context).clearSnackBars();
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(error.message ?? message),
-            backgroundColor: Theme.of(context).colorScheme.error,
-          ),
-        );
       }
+    } on FirebaseAuthException catch (error) {
+      var message = 'Authentication failed.';
+      if (error.code == 'email-already-in-use') {
+        // ...
+      }
+      ScaffoldMessenger.of(context).clearSnackBars();
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(error.message ?? message),
+          backgroundColor: Theme.of(context).colorScheme.error,
+        ),
+      );
     }
   }
 
