1369e302d849d6bcc850f546caf61bc76f9471d3
mnengineer <masashiii0115@gmail.com>
2023-09-02 20:17:10 +0900

    feat: チャットメッセージと入力Widgetの表示

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_messages.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -22,8 +24,13 @@ class ChatScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Loged in!'),
+      body: Column(
+        children: const [
+          Expanded(
+            child: ChatMessages(),
+          ),
+          NewMessage(),
+        ],
       ),
     );
   }
