b7505384f8f83e67ff1aabae4bd9bf19d2ef5f26
mnengineer <masashiii0115@gmail.com>
2023-09-02 21:30:48 +0900

    style: チャット画面のスタイル調整

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -9,7 +9,7 @@ class ChatMessages extends StatelessWidget {
     return StreamBuilder(
       stream: FirebaseFirestore.instance
           .collection('chat')
-          .orderBy('createdAt', descending: false)
+          .orderBy('createdAt', descending: true)
           .snapshots(),
       builder: (ctx, chatSnapshot) {
         if (chatSnapshot.connectionState == ConnectionState.waiting) {
@@ -31,10 +31,11 @@ class ChatMessages extends StatelessWidget {
         }
 
         return ListView.builder(
+          padding: const EdgeInsets.only(left: 13, right: 13, bottom: 40),
+          reverse: true,
           itemCount: chatSnapshot.data!.docs.length,
-          itemBuilder: (ctx, index) => Container(
-            padding: const EdgeInsets.all(8),
-            child: Text(chatSnapshot.data!.docs[index].data()['text']),
+          itemBuilder: (ctx, index) => Text(
+            chatSnapshot.data!.docs[index].data()['text'],
           ),
         );
       },
